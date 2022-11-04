b51ae520e7ef4a6de5f2bf1d4338abd79e375fc7
edgar ludena <edgar.ludena90@gmail.com>
2022-11-04 10:49:23 -0400

    commit tree 079ea407fd3a8b1091299a7143d1cafad5ab90b2

--- main.js
+++ main.js
@@ -1,25 +1,24 @@
+const { use } = require("chai")
+
 // Defining text characters for the empty and full hearts for you to use later.
 const EMPTY_HEART = '♡'
 const FULL_HEART = '♥'
 
 // Your JavaScript code goes here!
 
-
-
-
 //------------------------------------------------------------------------------
 // Don't change the code below: this function mocks the server response
 //------------------------------------------------------------------------------
 
-function mimicServerCall(url="http://mimicServer.example.com", config={}) {
-  return new Promise(function(resolve, reject) {
-    setTimeout(function() {
-      let isRandomFailure = Math.random() < .2
+function mimicServerCall(url = 'http://mimicServer.example.com', config = {}) {
+  return new Promise(function (resolve, reject) {
+    setTimeout(function () {
+      let isRandomFailure = Math.random() < 0.2
       if (isRandomFailure) {
-        reject("Random server error. Try again.");
+        reject('Random server error. Try again.')
       } else {
-        resolve("Pretend remote server notified of action!");
+        resolve('Pretend remote server notified of action!')
       }
-    }, 300);
-  });
+    }, 300)
+  })
 }
